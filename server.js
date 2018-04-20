3df0d1fab0a62303d9bb31a6661cd4736a78f87f
Amy <amysuelumpkin@gmail.com>
2018-04-20 12:28:35 -0400

    mongoose

--- server.js
+++ server.js
@@ -16,6 +16,25 @@ app.use(routes);
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
+//to connect to either heroku mlab or local mongodb
+/*const databaseUri = "mongodb://localhost/react-reading-list";
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect(databaseUri);
+}*/
+
+const db = mongoose.connection;
+
+db.on("error", function(err) {
+  console.log("Mongoose Error: ", err);
+});
+
+db.once("open", function() {
+  console.log("Mongoose connection successful");
+});
+
+
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
